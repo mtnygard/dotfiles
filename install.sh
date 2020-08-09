a7464e962c47a2df3981e23cdedef4d1b9a172dc
Michael Nygard <mtnygard@gmail.com>
2020-08-09 13:43:48 +0000

    Make an explicit post-clone installer

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+replace() {
+    [ -f $1 ] && rm $1
+    ln -s $2 $1
+}
+
+mkdir -p ~/.clojure
+mkdir -p ~/.m2
+
+replace ~/.gitconfig         dotfiles/gitconfig
+replace ~/.clojure/deps.edn  dotfiles/clojure/deps.edn
+replace ~/.m2/settings.xml   dotfiles/maven/settings.xml
