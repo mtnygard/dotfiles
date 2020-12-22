4a405db3f5dcaf45f410e49cafcc7243667d655a
Michael Nygard <mtnygard@gmail.com>
2020-12-22 10:12:09 -0600

    Also install keybindings that don't need CTRL-W

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ replace() {
 
 mkdir -p ~/.clojure
 mkdir -p ~/.m2
+mkdir -p ~/.vscode
 
-replace ~/.gitconfig         ~/dotfiles/gitconfig
-replace ~/.clojure/deps.edn  ~/dotfiles/clojure/deps.edn
-replace ~/.m2/settings.xml   ~/dotfiles/maven/settings.xml
+replace ~/.gitconfig               ~/dotfiles/gitconfig
+replace ~/.clojure/deps.edn        ~/dotfiles/clojure/deps.edn
+replace ~/.m2/settings.xml         ~/dotfiles/maven/settings.xml
+replace ~/.vscode/keybindings.json ~/dotfiles/vscode/keybindings.json
